f809edc478384847603c656ac94fb3b783942240
Grace Mwai <gracejmwai@gmail.com>
2026-06-20 16:02:28 +0300

    added hovering colour

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./Card.module.css";
 import LikeButton from "./LikeButton";
+import style from "./LikeButton.module.css";
 export default function Card({ name, blurb, rating, emoji, image }) {
  return (
 
      <article className={styles.card}>
      {image ? (
-        <Image src={image} alt={name} width={250} height={300} />
+        <Image src={image} alt={name} width={300} height={300} />
      ) : (
      <div className={styles.emoji}>{emoji}</div>
      )}
@@ -15,7 +16,7 @@ export default function Card({ name, blurb, rating, emoji, image }) {
      <p className={styles.stars}>{"⭐".repeat(rating)}</p>
      <div classname={styles.actions}> 
         
-        <LikeButton />
+        <LikeButton className= {style.LikeButton} />
      </div>
    </article>
 );
