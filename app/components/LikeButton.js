48a9b2ab570974afbc5df993ed2d296474079db9
Grace Mwai <gracejmwai@gmail.com>
2026-06-21 14:50:18 +0300

    added a button container

--- app/components/LikeButton.js
+++ app/components/LikeButton.js
@@ -6,8 +6,10 @@ export default function LikeButton() {
   const [liked, setLiked] = useState(false);
 
   return (
+    <div className="LikeButtonContainer">
     <button onClick={() => setLiked(!liked)}>
       {liked ? "❤️ Liked" : "🤍 Like"}
     </button>
+    </div>
   );
 }
